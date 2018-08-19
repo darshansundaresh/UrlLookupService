fedf9ef793f49ce9ec3d485a1798d507a871d0e4
Darshan Sundaresh <dharshan.ks@gmail.com>
2018-08-19 11:00:54 +0530

    initial checkin

---
+++ src/main/java/com/dx/urllookup/UrlLookupServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dx.urllookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlLookupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlLookupServiceApplication.class, args);
+	}
+}
