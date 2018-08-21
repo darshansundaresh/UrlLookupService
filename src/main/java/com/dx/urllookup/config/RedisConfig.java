95153b82450dfc8bf657b24ecfc69842fecc51fc
Darshan Sundaresh <dharshan.ks@gmail.com>
2018-08-21 12:42:09 +0530

    Dockerized the build
    Added new properties as redis connection factory wasnt referring to spring.redis.host and added dockerfile and compose yml

--- src/main/java/com/dx/urllookup/config/RedisConfig.java
+++ src/main/java/com/dx/urllookup/config/RedisConfig.java
@@ -1,18 +1,38 @@
 package com.dx.urllookup.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.lang.Nullable;
 
 @Configuration
 public class RedisConfig {
 
+	@Value("${redis.servername}")
+	private String redisServerName;
+	
+	@Value("${redis.serverport}")
+	private int redisServerPort;
+	
+	@Value("${redis.password:}")
+	private String redisPassword;
+	
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory(){
-		return new JedisConnectionFactory();
+		
+		 RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(this.redisServerName, this.redisServerPort);
+		    if(redisPassword!=null && redisPassword.length()>0) {
+		    	
+		    	redisStandaloneConfiguration.setPassword(RedisPassword.of(this.redisPassword));
+		    }
+		    return new JedisConnectionFactory(redisStandaloneConfiguration);
+		
 	}
 	
 	@Bean
